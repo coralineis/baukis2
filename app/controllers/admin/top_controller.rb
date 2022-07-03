4ff712998ddb8d0117498e135fbf399e3f20e346
coralineis <tavylrioly@gmail.com>
2022-07-03 16:07:56 +0900

    add 403forbidden エラーメッセージカスタム

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,5 +1,5 @@
 class Admin::TopController < ApplicationController
   def index
-    render action: "index"
+  　render action: "index"
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
   
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+  
   rescue_from StandardError, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAddressRejected, with: :rescue403
   
   private
   
@@ -11,6 +16,11 @@ class ApplicationController < ActionController::Base
     else
       "customer"
     end
+  end
+  
+  def rescue403(e)
+    @exception = e
+    render "errors/forbidden", status: 403
   end
   
   def rescue500(e)
