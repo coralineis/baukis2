a9c47d87f937f894e58663c3d49add00a4d1b953
coralineis <tavylrioly@gmail.com>
2022-07-11 13:51:56 +0900

    add admin ユーザー認証

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_03_115651) do
+ActiveRecord::Schema.define(version: 2022_07_09_141634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "administrators", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "hashed_password"
+    t.boolean "suspended", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index "lower((email)::text)", name: "index_administrators_on_LOWER_email", unique: true
+  end
 
   create_table "staff_members", force: :cascade do |t|
     t.string "email", null: false
--- db/seeds/development/staff_member.rb
+++
@@ -1,9 +0,0 @@
-StaffMember.create!(
-  email: "taro@example.com",
-  family_name: "山田",
-  given_name: "太郎",
-  family_name_kana: "ヤマダ",
-  given_name_kana: "タロウ",
-  password: "password",
-  start_date: Date.today
-)
