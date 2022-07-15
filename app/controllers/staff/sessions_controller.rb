3e3cbcc5ed22a2e74c7702de1d35ab848abb4b47
coralineis <tavylrioly@gmail.com>
2022-07-15 16:19:31 +0900

    add staff ログアウト機能

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -7,4 +7,23 @@ class Staff::SessionsController < Staff::Base
       render action: "new"
     end
   end
+  
+  def create
+    @form = Staff::LoginForm.new(params[:staff_login_form])
+    if @form.email.present?
+      staff_member = 
+        StaffMember.find_by("LOWER(email) = ?", @form.email.downcase)
+    end
+    if staff_member
+      session[:staff_member_id] = staff_member.id
+      redirect_to :staff_root
+    else
+      render :new
+    end
+  end
+  
+  def destroy
+    session.delete(:staff_member_id)
+    redirect_to :staff_root
+  end
 end
