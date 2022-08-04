93599ebcd8759c6e121d444c10d723901d692ed5
coralineis <tavylrioly@gmail.com>
2022-08-04 14:33:52 +0900

    add ログイン，アウト時フラッシュメッセージ

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -11,19 +11,21 @@ class Staff::SessionsController < Staff::Base
   def create
     @form = Staff::LoginForm.new(params[:staff_login_form])
     if @form.email.present?
-      staff_member = 
-        StaffMember.find_by("LOWER(email) = ?", @form.email.downcase)
+      staff_member = StaffMember.find_by("LOWER(email) = ?", @form.email.downcase)
     end
     if Staff::Authenticator.new(staff_member).authenticate(@form.password)
       session[:staff_member_id] = staff_member.id
+      flash.notice = "ログインしました。"
       redirect_to :staff_root
     else
+      flash.now.alert = "メールアドレスまたはパスワードが正しくありません。"
       render :new
     end
   end
   
   def destroy
     session.delete(:staff_member_id)
+    flash.notice = "ログアウトしました。"
     redirect_to :staff_root
   end
 end
