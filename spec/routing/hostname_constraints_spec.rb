7e43e425b9f03bf9a3d504d65dbe8157de1db26b
coralineis <tavylrioly@gmail.com>
2022-08-05 15:58:10 +0900

    9-4-3,9-5 ルーティングテスト

---
+++ spec/routing/hostname_constraints_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+describe "ルーティング" do
+  example "職員トップページ" do
+    config = Rails.application.config.baukis2
+    url = "http://#{config[:staff][:host]}/#{config[:staff][:path]}"
+    expect(get: url).to route_to(
+      host: config[:staff][:host],
+      controller: "staff/top",
+      action: "index"
+    )  
+  end
+
+  example "管理者ログインフォーム" do
+    config = Rails.application.config.baukis2
+    url = "http://#{config[:admin][:host]}/#{config[:admin][:path]}/login"
+    expect(get: url).to route_to(
+      host: config[:admin][:host],
+      controller: "admin/session",
+      action: "new"
+    )
+  end
+
+  example "顧客トップページ" do
+    config = Rails.application.config.baukis2
+    url = "http://#{config[:customer][:host]}/#{config[:customer][:path]}"
+    expect(get: url).to route_to(
+      host: config[:customer][:host],
+      controller: "customer/top"
+      adtion: "index"
+    )  
+  end
+
+  example "ホスト名が対象外ならroutableではない" do
+    expect(get: "http://foo.example.jp").not_to be_routable  
+  end
+
+  example "存在しないパスならroutableではない" do
+    config = Rails.application.config.baukis2
+    expect(get: "http://#{config[:staff][:host]}/xyz").not_to be_routable  
+  end
+end
