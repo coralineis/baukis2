ba62da97d6e24da143a942770c6a36e26554f44d
coralineis <tavylrioly@gmail.com>
2022-08-07 15:21:31 +0900

    11-1-1~3

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -9,7 +9,7 @@ class Admin::SessionsController < Admin::Base
   end
 
   def create
-    @form = Admin::LoginForm.new(params[:admin_login_form])
+    @form = Admin::LoginForm.new(login_form_params)
     if @form.email.present?
       administrator = Administrator.find_by("LOWER(email) = ?", @form.email.downcase)
     end
@@ -32,5 +32,11 @@ class Admin::SessionsController < Admin::Base
     session.delete(:administrator_id)
     flash.notice = "ログアウトしました。"
     redirect_to :admin_root
+  end
+
+  private
+
+  def login_form_params
+    params.require(:admin_login_form).permit(:email, :password)
   end
 end
--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -9,7 +9,7 @@ class Staff::SessionsController < Staff::Base
   end
   
   def create
-    @form = Staff::LoginForm.new(params[:staff_login_form])
+    @form = Staff::LoginForm.new(login_form_params)
     if @form.email.present?
       staff_member = StaffMember.find_by("LOWER(email) = ?", @form.email.downcase)
     end
@@ -32,5 +32,11 @@ class Staff::SessionsController < Staff::Base
     session.delete(:staff_member_id)
     flash.notice = "ログアウトしました。"
     redirect_to :staff_root
+  end
+
+  private
+
+  def login_form_params
+    params.require(:staff_login_form).permit(:email, :password)
   end
 end
