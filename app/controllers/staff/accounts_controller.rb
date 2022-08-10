a8c6e6e9778ca768d0abdbd6cdf80aeb52b3dfe1
coralineis <tavylrioly@gmail.com>
2022-08-10 21:28:56 +0900

    11-2-2,3

--- app/controllers/staff/accounts_controller.rb
+++ app/controllers/staff/accounts_controller.rb
@@ -3,4 +3,27 @@ class Staff::AccountsController < Staff::Base
     @staff_member = current_staff_member
   end
   
+  def edit
+    @staff_member = current_staff_member
+  end
+  
+  def update
+    @staff_member = current_staff_member
+    @staff_member.assign_attributes(staff_member.params)
+    if @staff_member.save
+      flash.notice = "アカウント情報を更新しました。"
+      redirect_to :staff_account
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def staff_member_params
+      params.require(:staff_member).permit(
+        :email, :family_name, :given_name, :family_name_kana, given_name_kana
+      )
+    end
+    
 end
